dada8f476a21337bb28a4d5e6209098738809fe9
syahruaru <57306432+syahruaru@users.noreply.github.com>
2022-11-09 07:50:11 +0700

    Add files via upload

---
+++ Get_data.py
@@ -0,0 +1,10 @@
+import os
+from bs4 import BeautifulSoup
+#from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeout
+import time
+
+#%pip install playwright
+# Run from your project's root directory
+npm init playwright@latest
+# Or create a new project
+npm init playwright@latest new-project
